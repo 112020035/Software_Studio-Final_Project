6ea8a1afb9f1095bd9fb718b550bbb2949d9570b
joanna-taiwanese <joanna940523@gmail.com>
2026-06-09 21:06:51 +0800

    setting

--- assets/scripts/ui/MainMenuCtrl.ts
+++ assets/scripts/ui/MainMenuCtrl.ts
@@ -18,50 +18,57 @@ export default class MainMenuCtrl extends cc.Component {
         // 播放BGM
         AudioBroadcast.playBgm("main_menu_bgm");
         
-        const handler = new cc.Component.EventHandler();
-        handler.target = this.node;
-        handler.component = "MainMenuCtrl";
-        handler.handler = "onStartGame";
-
         const startBtn = cc.find("Canvas/StartButton");
-        const settingsBtn = cc.find("Canvas/SettingsButton");
+        const settingBtn = cc.find("Canvas/SettingsButton");
         if (startBtn) {
             const button = startBtn.getComponent(cc.Button);
-            
+
             // btn_press 音效
             const soundHandler = new cc.Component.EventHandler();
             soundHandler.target = this.node;
             soundHandler.component = "MainMenuCtrl";
             soundHandler.handler = "onBtnPress";
             button.clickEvents.push(soundHandler);
-            
+
             // 換場景
+            const handler = new cc.Component.EventHandler();
+            handler.target = this.node;
+            handler.component = "MainMenuCtrl";
+            handler.handler = "onStartGame";
             button.clickEvents.push(handler);
-        } else if (settingsBtn){
-            const button = settingsBtn.getComponent(cc.Button);
-            
-            // btn_press 音效
-            const soundHandler = new cc.Component.EventHandler();
-            soundHandler.target = this.node;
-            soundHandler.component = "MainMenuCtrl";
-            soundHandler.handler = "onBtnPress";
-            button.clickEvents.push(soundHandler);
-            
-            // 換場景
-            const settingsHandler = new cc.Component.EventHandler();
-            settingsHandler.target = this.node;
-            settingsHandler.component = "MainMenuCtrl";
-            settingsHandler.handler = "onSettings";
-            button.clickEvents.push(settingsHandler);
-            
         } else {
             cc.warn("MainMenuCtrl: 找不到 Canvas/StartButton");
         }
-            }
+
+        if (settingBtn) {
+            const button = settingBtn.getComponent(cc.Button);
+
+            // btn_press 音效
+            const soundHandler = new cc.Component.EventHandler();
+            soundHandler.target = this.node;
+            soundHandler.component = "MainMenuCtrl";
+            soundHandler.handler = "onBtnPress";
+            button.clickEvents.push(soundHandler);
+
+            // 換場景
+            const handler = new cc.Component.EventHandler();
+            handler.target = this.node;
+            handler.component = "MainMenuCtrl";
+            handler.handler = "onOpenSetting";
+            button.clickEvents.push(handler);
+        } else {
+            cc.warn("MainMenuCtrl: 找不到 Canvas/SettingsButton");
+        }
+    }
 
     onStartGame() {
         cc.director.loadScene("Intro");
     }
+
+    onOpenSetting() {
+        cc.director.loadScene("Setting");
+    }
+
     // btn_press的音效
     onBtnPress() {
         AudioBroadcast.playEffect("btn_press");
--- assets/scripts/ui/SettingCtrl.ts
+++ assets/scripts/ui/SettingCtrl.ts
@@ -5,27 +5,60 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class SettingsCtrl extends cc.Component {
 
-    @property(cc.Slider)
-    bgmSlider: cc.Slider = null;
+    @property(cc.Button)
+    backButton: cc.Button = null!;
 
     @property(cc.Slider)
-    effectSlider: cc.Slider = null;
+    bgmSlider: cc.Slider = null!;
+
+    @property(cc.Slider)
+    effectSlider: cc.Slider = null!;
 
     start() {
-        // 初始化滑桿位置對應當前音量
+        if (this.backButton) {
+            this.backButton.node.on("click", this.onBackButtonClick, this);
+        } else {
+            cc.warn("SettingCtrl: 請在 Inspector 掛上 Back Button");
+        }
+
+        // 初始化滑動條位置與事件
         if (this.bgmSlider) {
             this.bgmSlider.progress = AudioBroadcast.getBgmVolume();
+            this.bgmSlider.node.on("slide", () => {
+                AudioBroadcast.setBgmVolume(this.bgmSlider.progress);
+                // cc.log("BGM volume =", this.bgmSlider.progress);
+            }, this);
         }
+
         if (this.effectSlider) {
             this.effectSlider.progress = AudioBroadcast.getEffectVolume();
+            this.effectSlider.node.on("slide", () => {
+                AudioBroadcast.setEffectVolume(this.effectSlider.progress);
+                // cc.log("Effect volume =", this.effectSlider.progress);
+            }, this);
         }
     }
 
     onBgmSliderChanged() {
         AudioBroadcast.setBgmVolume(this.bgmSlider.progress);
+        // cc.log("SettingsCtrl: BGM volume set to " + this.bgmSlider.progress);
     }
 
     onEffectSliderChanged() {
         AudioBroadcast.setEffectVolume(this.effectSlider.progress);
+        // cc.log("SettingsCtrl: Effect volume set to " + this.effectSlider.progress);
+    }
+
+    onBackButtonClick() {
+        this.onBtnPress();
+        this.onBackToMainMenu();
+    }
+
+    onBackToMainMenu() {
+        cc.director.loadScene("MainMenu");
+    }
+
+    onBtnPress() {
+        AudioBroadcast.playEffect("btn_press");
     }
 }
